7e207c74d9772dfd21553430bb6be7c1af4cf43f
Nico <nico.macioszek@gmail.com>
2023-10-09 12:31:45 +0200

    Readded restart

--- VotingTrackModule.cs
+++ VotingTrackModule.cs
@@ -2,6 +2,7 @@
 using Autofac;
 using Microsoft.Extensions.DependencyInjection;
 using nvrlift.AssettoServer.ContentManager;
+using nvrlift.AssettoServer.Restart;
 using nvrlift.AssettoServer.Track;
 
 namespace VotingTrackPlugin;
@@ -11,7 +12,7 @@ public class VotingTrackModule : AssettoServerModule<VotingTrackConfiguration>
     protected override void Load(ContainerBuilder builder)
     {
         // Register Base Stuff
-        // builder.RegisterType<WindowsFileRestartImplementation>().As<IRestartImplementation>().SingleInstance();
+        builder.RegisterType<WindowsFileRestartImplementation>().As<IRestartImplementation>().SingleInstance();
         builder.RegisterType<TrackImplementation>().AsSelf().SingleInstance();
         builder.RegisterType<TrackManager>().AsSelf().SingleInstance();
         builder.RegisterType<ContentManagerImplementation>().AsSelf().SingleInstance();
