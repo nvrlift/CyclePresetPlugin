c032e6045544cf19041e1e88bedb9c54890975a7
Nico <nico.macioszek@gmail.com>
2023-10-15 03:09:49 +0200

    IT BE WORKIN MAN

--- lua/reconnectclient.lua
+++ lua/reconnectclient.lua
@@ -1,4 +1,5 @@
 local pleaseReconnect = false
+local drawReconnect = false
 local reconnectDelay = 0
 local reconnectClientEvent = ac.OnlineEvent(
     { -- DONT CHANGE THIS STRUCT OR YOU NEED TO GET A NEW ID FOR THE PACKET.CS
@@ -7,19 +8,19 @@ local reconnectClientEvent = ac.OnlineEvent(
     }, function(sender, message)
         if sender ~= nil then return end
         
-        pleaseReconnect = true
+        -- pleaseReconnect = true
+        drawReconnect = true
         reconnectDelay = message.time
     end)
 
 local centerPos = nil
 function script.drawUI()
-    if pleaseReconnect then
-        ac.sendChatMessage("pleaseReconnect script.drawUI()")
+    if drawReconnect then
         if centerPos == nil then
-            centerPos = vec2(ac.getUI().windowSize.x / 2, 100)
+            centerPos = vec2((ac.getUI().windowSize.x - 800) / 2, ac.getUI().windowSize.y / 2 - 100)
         end
-
-        ui.beginTransparentWindow("reconnectClient", centerPos, vec2(400 * 0.5, 400 * 0.5))
+        
+        ui.beginTransparentWindow("reconnectClient", centerPos, vec2(800, 200))
         ui.beginOutline()
 
         ui.pushFont(ui.Font.Huge)
@@ -33,12 +34,21 @@ function script.drawUI()
     end
 end
 
+function sleep(n)  -- seconds
+    --[[local t0 = os.clock()
+    while os.clock() - t0 <= n do end]]
+
+    local ntime = os.clock() + n -- / 10
+    repeat until os.clock() > ntime
+end
+
 function script.update(dt)
-    if pleaseReconnect then
+    if (drawReconnect == true and pleaseReconnect == false) then
+        pleaseReconnect = true
+    elseif pleaseReconnect then
         pleaseReconnect = false
-        ac.sendChatMessage("pleaseReconnect script.update(dt)")
-        
-        worker.sleep(reconnectDelay)
+        sleep(reconnectDelay)
+        -- drawReconnect = false
         ac.reconnectTo({ carID = ac.getCarID(0) })
     end
 end
